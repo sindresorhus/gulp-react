15c19ae2e467a1ec4e8663d4a80a10c2b0e798f3
Kevin Martensson <kevinmartensson@gmail.com>
2015-02-23 22:38:03 +0100

    Add support for sourcemaps
    Fixes #18.

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
 var assert = require('assert');
 var gutil = require('gulp-util');
+var sourceMaps = require('gulp-sourcemaps');
 var react = require('./');
 
 it('should precompile React templates', function (cb) {
@@ -31,4 +32,31 @@ it('should precompile React templates with --harmony flag', function (cb) {
 		path: 'fixture.harmony.jsx',
 		contents: new Buffer('/** @jsx React.DOM */var HelloMessage = React.createClass({render: () => {return <div>Hello {this.props.name}</div>;}});')
 	}));
+});
+
+it('should generate source maps', function (cb) {
+	var init = sourceMaps.init();
+	var write = sourceMaps.write();
+
+	init
+		.pipe(react())
+		.pipe(write);
+
+	write.on('data', function (file) {
+		var contents = file.contents.toString();
+		assert.equal(file.sourceMap.sources[0], 'fixture.jsx');
+		assert(/React.createElement/.test(contents));
+		assert(/sourceMappingURL=data:application\/json;base64/.test(contents));
+		cb();
+	});
+
+	init.write(new gutil.File({
+		cwd: __dirname,
+		base: __dirname,
+		path: 'fixture.jsx',
+		contents: new Buffer('/** @jsx React.DOM */var HelloMessage = React.createClass({render: function(){return <div>Hello {this.props.name}</div>;}});'),
+		sourceMap: ''
+	}));
+
+	init.end();
 });
